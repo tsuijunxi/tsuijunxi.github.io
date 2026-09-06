e683a63964ee0b1804dd6411d4f6c26c5c870fcf
tsuijunxi <598395670@qq.com>
2026-09-06 23:54:27 +0800

    Site updated: 2026-09-06 23:54:26

--- js/src/utils.js
+++ js/src/utils.js
@@ -7,7 +7,7 @@ NexT.utils = NexT.$u = {
   wrapImageWithFancyBox: function () {
     $('.content img')
       .not('[hidden]')
-      .not('.group-picture img, .post-gallery img')
+      .not('.group-picture img, .post-gallery img, .post-cover img')
       .each(function () {
         var $image = $(this);
         var imageTitle = $image.attr('title');
